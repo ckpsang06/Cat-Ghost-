6cbbf998f82f5f7160897e3be697fd887448392e
tantung6 <nguyenphuctanvt2006@gmail.com>
2026-07-27 17:29:44 +0700

    them chuc nang menu ver1

---
+++ CommonFunc.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <bits/stdc++.h>
+#include <SDL.h>
+
+using namespace std;
+
+const int SCREEN_WIDTH = 1224;
+const int SCREEN_HEIGHT = 800;
+
+const int FPS = 60;
+const int frameDelay = 1000 / FPS;
+
+const SDL_Color whiteColor = {255, 255, 255, 255};
+const SDL_Color blackColor = {0, 0, 0, 255};
+
+const int CatHealth = 5;
---
+++ Game.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include "Entity.h"
+#include "mainObject.h"
+
+enum GameStateName {
+    GameOpenning,
+    GamePlaying,
+    GameEnding,
+    TotalGameState
+};
+
+enum GameTextureName {
+    Menu1Texture,
+    Menu1_2Texture,
+    BackGroundTexture,
+    CatTexture,
+    HeartTexture,
+    TotalGameTexutre
+};
+
+enum CatStateName {
+    CatIdle,
+    TotalCatState
+};
+
+class Game {
+private:
+    bool isRunning;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+    SDL_Texture* GameTexture[TotalGameTexutre] = {NULL};
+    Cat cat[TotalCatState];
+    Entity heart;
+
+    int GameState;
+public:
+    Game();
+    void initSDL(const char *p_title, int p_w, int p_h);
+    void loadMedia();
+    void initEntity();
+    SDL_Texture *loadTexture(const char *p_filePath);
+
+    bool running();
+    void clear();
+    void handleEvents();
+    void gameUpdate();
+    void render(SDL_Texture *p_tex);
+    void render(Entity &p_entity, SDL_Rect src = {0, 0, 0, 0});
+    void gameRender();
+    void cleanUp();
+    void gameLoop();
+};
